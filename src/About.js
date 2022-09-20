472ed6ad8ab1b86b78863e8556baa0b2a3939dec
WalaaAtiah <walaaatiah97@gmail.com>
2022-09-20 12:44:39 +0300

    full

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
 
+
 class About extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return <h4 className="about" style={{textAlign:"center",padding:"60px 150px"}}>Books are life-changing. They have the power to enlighten, educate, entertain, heal, and help us grow. Builded out this code base to create an web app to track what books have impacted you, and what's recommended to read next.</h4>
   }
 };
 
